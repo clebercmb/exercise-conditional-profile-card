a3800610cdf031cccc7cebb1970613f37f2243b4
Cleber Miranda Barbosa <cleber@gmail.com>
2019-11-25 19:19:19 -0300

    Conditional Profile Card - final deliver

--- public/bundle.js
+++ public/bundle.js
@@ -701,30 +701,35 @@ __webpack_require__.r(__webpack_exports__);
  *  This function is called every time the user changes types or changes any input
  */
 function render(variables = {}) {
-  // here we ask the logical questions to make decitions on how to build the heml
+  // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+
+  // you will have to replace the name, lastname, etc. With the values that these variables may have.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+
+  if (variables.name == null) variables.name = "Your name";
+  if (variables.lastname == null) variables.lastname = "Your lastname";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <img src=${variables.avatarURL} class="photo" />
+          <h1>${variables.name}  ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href=${variables.twitter}><i class="fa fa-twitter"></i></a></li>
+            <li><a href=${variables.github}<i class="fa fa-github"></i></a></li>
+            <li><a href=${variables.linkedin}><i class="fa fa-linkedin"></i></a></li>
+            <li><a href=${variables.instagram}><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
 }
 
 /**
- * Ignore this lines below, here is where we do the logic for the dropdowns
+ * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
 window.onload = function () {
   window.variables = {
@@ -735,7 +740,7 @@ window.onload = function () {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "left",
+    socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
     github: "alesanchezr",
@@ -747,7 +752,9 @@ window.onload = function () {
     country: null,
     city: null
   };
+
   render(window.variables);
+
   document.querySelectorAll(".picker").forEach(function (elm) {
     elm.addEventListener("change", function (e) {
       const attribute = e.target.getAttribute("for");
